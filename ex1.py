b18796b5a4f7d189ecd1985aaab4f21c98f6b6cf
baddaswicked <63231651+baddaswicked@users.noreply.github.com>
2024-03-13 01:58:34 +0200

    new_comit

---
+++ ex1.py
@@ -0,0 +1,22 @@
+def total_salary(path):
+    try:
+        with open(path,"r") as file:
+            lst=[]
+            for line in file:
+                name,salary=line.split(",")
+                lst.append(int(salary))
+
+        return (sum(lst), sum(lst)//len(lst))
+    except FileExistsError:
+        print("Файл не знайдено")
+    except Exception as e:
+        print("Помилка: ",e)
+# print(total_salary("my_text.txt"))
+total, average = total_salary("my_text.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+
+
+
+
+
