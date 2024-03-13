107ca950b4844fb81cea152d6d34ddc59e4b8a81
baddaswicked <63231651+baddaswicked@users.noreply.github.com>
2024-03-13 02:06:08 +0200

    new_comit

--- ex2.py
+++ ex2.py
@@ -1,12 +1,17 @@
 def get_cats_info(path):
     list=[]
-    with open(path, "r") as file:
-        for line in file:
-            data=line.strip().split(",")
-            list.append({
-                "id":data[0],
-                "name":data[1],
-                "age":data[2]
-            })
-    return list
+    try:
+        with open(path, "r") as file:
+            for line in file:
+                data=line.strip().split(",")
+                list.append({
+                    "id":data[0],
+                    "name":data[1],
+                    "age":data[2]
+                })
+        return list
+    except FileExistsError:
+        print("Файл не знайдено")
+    except Exception as e:
+        print("Помилка: ",e)
 print(get_cats_info("my_text.txt"))
