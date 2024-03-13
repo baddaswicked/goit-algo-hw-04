5833b4b6f45264baf07a10b27b4255937d31f90d
baddaswicked <63231651+baddaswicked@users.noreply.github.com>
2024-03-13 02:04:32 +0200

    new_comit

--- ex2.py
+++ ex2.py
@@ -1,2 +1,12 @@
 def get_cats_info(path):
+    list=[]
     with open(path, "r") as file:
+        for line in file:
+            data=line.strip().split(",")
+            list.append({
+                "id":data[0],
+                "name":data[1],
+                "age":data[2]
+            })
+    return list
+print(get_cats_info("my_text.txt"))
